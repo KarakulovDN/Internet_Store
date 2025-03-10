d400bd74d58ab83333b2c785faf39271e5c31826
Karakulov Dmitriy <karakulovdn@gmail.com>
2025-03-10 11:51:48 +0700

    my_string из списка в строку

--- tests/test_category.py
+++ tests/test_category.py
@@ -10,27 +10,26 @@ def test_category_smart(category_smart: Any) -> Any:
     assert category_smart.name == "Смартфоны"
     assert (category_smart.description == "Смартфоны, как средство не только коммуникации, но и"
             " получения дополнительных функций для удобства жизни")
-    assert category_smart.products == ["Samsung Galaxy S23 Ultra",
-                                       "Iphone 15", "Xiaomi Redmi Note 11"]
+    assert category_smart.products == 'Samsung Galaxy S23 Ultra, Iphone 15, Xiaomi Redmi Note 11'
 
 
 def test_category_tv(category_tv: Any) -> Any:
     assert category_tv.name == "Телевизоры"
     assert (category_tv.description == "Современный телевизор, который позволяет"
             " наслаждаться просмотром, станет вашим другом и помощником")
-    assert category_tv.products == ["Samsung", "Xiaomi", "Toshiba"]
+    assert category_tv.products == 'Samsung, Xiaomi, Toshiba'
 
 
-def test_category_count_tv(category_tv: Any) -> Any:
-    assert len(category_tv.products) == 3
-
-
-def test_category_count_smart(category_smart: Any) -> Any:
-    assert len(category_smart.products) == 3
-
-
-def test_all_count(category_smart: Any, category_tv: Any) -> Any:
-    assert len(category_smart.products) + len(category_tv.products) == 6
+# def test_category_count_tv(category_tv: Any) -> Any:
+#     assert len(category_tv.products) == 3
+#
+#
+# def test_category_count_smart(category_smart: Any) -> Any:
+#     assert len(category_smart.products) == 3
+#
+#
+# def test_all_count(category_smart: Any, category_tv: Any) -> Any:
+#     assert len(category_smart.products) + len(category_tv.products) == 6
 
 
 def test_product_smart(product_smart) -> Any:
@@ -51,7 +50,7 @@ def test_add_product_to_existing_list(product1_fixture: Product, product2_fixtur
     """Проверяем работу метода add_product для случая, когда добавляется список продуктов и продукт в конструктор."""
     test_category = Category(name="Продукты", description="Товары первой необходимости", products=[product1_fixture])
     test_category.add_product(product2_fixture)
-    assert test_category.products == ["Молоко, 500.0 руб. Остаток: 5 шт.", "Хлеб, 100.0 руб. Остаток: 3 шт."]
+    assert test_category.products == 'Молоко, 500.0 руб. Остаток: 5 шт., Хлеб, 100.0 руб. Остаток: 3 шт.'
 
 
 def test_add_product_if_invalid_arg(product1_fixture: Product) -> None:
@@ -68,11 +67,11 @@ def test_category_getter() -> None:
     category = Category(
         name="Электроника", description="Электроника и устройства для дома", products=[product1, product2]
     )
-    expected_result = ["Ноутбук, 80000 руб. Остаток: 15 шт.", "Смартфон, 60000 руб. Остаток: 10 шт."]
+    expected_result = 'Ноутбук, 80000 руб. Остаток: 15 шт., Смартфон, 60000 руб. Остаток: 10 шт.'
     assert category.products == expected_result
 
 
 def test_products_property_empty() -> None:
     """Проверяем работу геттера когда список продуктов пустой."""
     category = Category(name="Книги", description="Книги всех жанров")
-    assert category.products == []
+    assert category.products == ''
